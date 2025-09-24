7909c7ab64f992b116dce7b56d07f09cc99b9f45
Mitchell-Mashburn <mitchell@re3d.org>
2025-09-24 13:35:32 -0500

    Testing home directory

--- src/modules/fullpageos/filesystem/home/pi/printer_data/config/src/system_config/shell_commands/announcement.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/moonlight
-source .venv/bin/activate
-src/update_rss.py
-deactivate
--- src/modules/fullpageos/filesystem/home/pi/printer_data/config/src/system_config/shell_commands/mount_usb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mount -tvfat -osync,noexec,nodev,noatime,nodiratime,nosuid,fmask=0,dmask=0 /dev/sda1 /media/usb0

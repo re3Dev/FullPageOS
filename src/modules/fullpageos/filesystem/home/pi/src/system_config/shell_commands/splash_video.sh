cb4923ec2ddd9f8e15043544d1e6596686436555
Mitchell-Mashburn <mitchell@re3d.org>
2025-10-03 14:20:05 -0500

    Add splash_video.sh

---
+++ src/modules/fullpageos/filesystem/home/pi/src/system_config/shell_commands/splash_video.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 15
+export DISPLAY=:0
+export XAUTHORITY=/home/pi/.Xauthority
+/usr/bin/cvlc --play-and-exit --no-video-deco --no-embedded-video /home/pi/re3D_logo_animation.mp4 
+

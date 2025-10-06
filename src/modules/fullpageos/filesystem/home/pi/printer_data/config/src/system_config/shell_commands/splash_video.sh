52127f2a0b922ee4e64e87876ebe8d9457d8bed4
Mitchell-Mashburn <mitchell@re3d.org>
2025-10-06 11:03:59 -0500

    splash video fullscreen and hide file name

--- src/modules/fullpageos/filesystem/home/pi/printer_data/config/src/system_config/shell_commands/splash_video.sh
+++ src/modules/fullpageos/filesystem/home/pi/printer_data/config/src/system_config/shell_commands/splash_video.sh
@@ -2,5 +2,5 @@
 sleep 15
 export DISPLAY=:0
 export XAUTHORITY=/home/pi/.Xauthority
-/usr/bin/cvlc --play-and-exit --no-video-deco --no-embedded-video /home/pi/re3D_logo_animation.mp4 
+/usr/bin/cvlc --play-and-exit --no-video-deco --no-embedded-video --no-video-title-show --fullscreen /home/pi/re3D_logo_animation.mp4 
 
